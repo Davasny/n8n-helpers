fdcad1d796a13dfb816a5570f73e9fb3fc36aeb1
wjkba <63542318+wjkba@users.noreply.github.com>
2025-10-14 16:02:35 +0200

    feat(converter): excel to csv

---
+++ src/features/convert/router.ts
@@ -0,0 +1,43 @@
+import { vValidator } from "@hono/valibot-validator";
+import { Hono } from "hono";
+import * as v from "valibot";
+import { convertExcelToCsv } from "./convertExcelToCsv";
+
+export const convertApp = new Hono();
+
+const conversionSchema = v.object({
+  from: v.pipe(v.string(), v.maxLength(4)),
+  to: v.pipe(v.string(), v.length(3)),
+  file: v.object({
+    name: v.pipe(v.string(), v.minLength(1)),
+    base64: v.pipe(v.string(), v.base64()),
+  }),
+});
+
+convertApp.post(
+  "/convert/base64",
+  vValidator("json", conversionSchema),
+  (c) => {
+    const { from, to, file } = c.req.valid("json");
+    const { name, base64 } = file;
+
+    if ((from === "xlsx" || from === "xls") && to === "csv") {
+      const csv = convertExcelToCsv(base64);
+
+      return c.json(
+        {
+          data: csv,
+          filename: name.replace(/\.(xlsx|xls)$/, ".csv"),
+        },
+        200
+      );
+    }
+
+    return c.json(
+      {
+        message: `Conversion from ${from} to ${to} is not supported`,
+      },
+      400
+    );
+  }
+);
